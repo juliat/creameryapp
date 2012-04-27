39988d9a19a4e7945e87a26a0a7fe9d6897bb72d
Julia Teitelbaum <juliat@cmu.edu>
2012-04-27 12:36:51 -0400

    got createadmin migration working

--- db/migrate/20120425040811_create_admin.rb
+++ db/migrate/20120425040811_create_admin.rb
@@ -1,8 +1,18 @@
 class CreateAdmin < ActiveRecord::Migration
   # add default user admin to system when migration is run
   def up
+    employee = Employee.new
+    employee.first_name = "Julia"
+    employee.last_name = "Teitelbaum"
+    employee.role = "admin"
+    employee.date_of_birth = "1992-01-21"
+    employee.phone = "3038153575"
+    employee.ssn = "123456789"
+    employee.save!
+            
     admin = User.new
-    admin.email = "julialt@gmail.com"
+    admin.employee_id = employee.id
+    admin.email = "juliat@gmail.com"
     admin.password ="opensesame"
     admin.password_confirmation = "opensesame"
     admin.save!
@@ -10,6 +20,9 @@ class CreateAdmin < ActiveRecord::Migration
   
   # and delete that user when cleaning up migration
   def down
+    employee = Employee.find_by_phone "3038153575"
+    Employee.delete employee
+    
     admin = User.find_by_email "julialt@gmail.com"
     User.delete admin
   end
