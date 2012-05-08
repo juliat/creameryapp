da41ce713c7f556793931084751e791716bfaf0a
Julia Teitelbaum <juliat@cmu.edu>
2012-05-07 23:40:56 -0400

    trying to figure out why shift dates won't save

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -24,6 +24,7 @@ class ShiftsController < ApplicationController
 
 	def create
 		@shift = Shift.new(params[:shift])
+		@shift.date =  Chronic.parse(params[:shift][:date])
 		if @shift.save
 			# if saved to database
 			flash[:notice] = "Successfully created this shift."
@@ -36,6 +37,7 @@ class ShiftsController < ApplicationController
 
 	def update
 		@shift = Shift.find(params[:id])
+		@shift.date =  Chronic.parse(params[:shift][:date])
 		if @shift.update_attributes(params[:shift])
 			flash[:notice] = "Successfully updated this shift."
 			redirect_to @shift
